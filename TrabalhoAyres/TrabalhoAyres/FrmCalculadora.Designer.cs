34579e3a30f359f45af97942a55e7f3c30f11d1a
feayres <ayresf.dev@gmail.com>
2024-09-21 20:05:49 -0300

    Add files via upload

---
+++ TrabalhoAyres/TrabalhoAyres/FrmCalculadora.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace TrabalhoAyres
+{
+    partial class FrmCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button5 = new Button();
+            button8 = new Button();
+            button4 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button7 = new Button();
+            button3 = new Button();
+            button6 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            groupBox1 = new GroupBox();
+            buttonenter = new Button();
+            buttonC = new Button();
+            buttondivisão = new Button();
+            buttonvezes = new Button();
+            buttonmenos = new Button();
+            buttonsoma = new Button();
+            Resultado_Text = new Label();
+            groupBox2 = new GroupBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button5
+            // 
+            button5.Location = new Point(172, 149);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 37);
+            button5.TabIndex = 2;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(172, 61);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 38);
+            button8.TabIndex = 3;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(6, 149);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 37);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(6, 241);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 39);
+            button1.TabIndex = 5;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(172, 241);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 39);
+            button2.TabIndex = 6;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(6, 61);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 38);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(339, 241);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 39);
+            button3.TabIndex = 8;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(339, 149);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 37);
+            button6.TabIndex = 9;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(339, 61);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 38);
+            button9.TabIndex = 10;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button0
+            // 
+            button0.Location = new Point(172, 326);
+            button0.Name = "button0";
+            button0.Size = new Size(94, 43);
+            button0.TabIndex = 11;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            button0.Click += button0_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.ControlDarkDark;
+            groupBox1.Controls.Add(buttonenter);
+            groupBox1.Controls.Add(buttonC);
+            groupBox1.Controls.Add(buttondivisão);
+            groupBox1.Controls.Add(buttonvezes);
+            groupBox1.Controls.Add(buttonmenos);
+            groupBox1.Controls.Add(buttonsoma);
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button0);
+            groupBox1.Controls.Add(button8);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button9);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(12, 122);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(623, 386);
+            groupBox1.TabIndex = 12;
+            groupBox1.TabStop = false;
+            // 
+            // buttonenter
+            // 
+            buttonenter.Location = new Point(339, 329);
+            buttonenter.Name = "buttonenter";
+            buttonenter.Size = new Size(94, 36);
+            buttonenter.TabIndex = 17;
+            buttonenter.Text = "ENTER";
+            buttonenter.UseVisualStyleBackColor = true;
+            buttonenter.Click += buttonenter_Click;
+            // 
+            // buttonC
+            // 
+            buttonC.Location = new Point(6, 325);
+            buttonC.Name = "buttonC";
+            buttonC.Size = new Size(94, 44);
+            buttonC.TabIndex = 16;
+            buttonC.Text = "C";
+            buttonC.UseVisualStyleBackColor = true;
+            buttonC.Click += buttonC_Click;
+            // 
+            // buttondivisão
+            // 
+            buttondivisão.Location = new Point(520, 325);
+            buttondivisão.Name = "buttondivisão";
+            buttondivisão.Size = new Size(94, 44);
+            buttondivisão.TabIndex = 15;
+            buttondivisão.Text = "/";
+            buttondivisão.UseVisualStyleBackColor = true;
+            buttondivisão.Click += buttondivisão_Click;
+            // 
+            // buttonvezes
+            // 
+            buttonvezes.Location = new Point(520, 241);
+            buttonvezes.Name = "buttonvezes";
+            buttonvezes.Size = new Size(94, 39);
+            buttonvezes.TabIndex = 14;
+            buttonvezes.Text = "*";
+            buttonvezes.UseVisualStyleBackColor = true;
+            buttonvezes.Click += buttonvezes_Click;
+            // 
+            // buttonmenos
+            // 
+            buttonmenos.Location = new Point(520, 149);
+            buttonmenos.Name = "buttonmenos";
+            buttonmenos.Size = new Size(94, 37);
+            buttonmenos.TabIndex = 13;
+            buttonmenos.Text = "-";
+            buttonmenos.UseVisualStyleBackColor = true;
+            buttonmenos.Click += buttonmenos_Click;
+            // 
+            // buttonsoma
+            // 
+            buttonsoma.Location = new Point(520, 61);
+            buttonsoma.Name = "buttonsoma";
+            buttonsoma.Size = new Size(94, 38);
+            buttonsoma.TabIndex = 12;
+            buttonsoma.Text = "+";
+            buttonsoma.UseVisualStyleBackColor = true;
+            buttonsoma.Click += buttonsoma_Click;
+            // 
+            // Resultado_Text
+            // 
+            Resultado_Text.AutoSize = true;
+            Resultado_Text.Location = new Point(265, 53);
+            Resultado_Text.Name = "Resultado_Text";
+            Resultado_Text.Size = new Size(0, 31);
+            Resultado_Text.TabIndex = 13;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = SystemColors.ControlDark;
+            groupBox2.Controls.Add(Resultado_Text);
+            groupBox2.Location = new Point(12, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(623, 125);
+            groupBox2.TabIndex = 14;
+            groupBox2.TabStop = false;
+            // 
+            // FrmCalculadora
+            // 
+            AutoScaleDimensions = new SizeF(13F, 31F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(647, 530);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5);
+            Name = "FrmCalculadora";
+            Text = "FrmCalculadora";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button button5;
+        private Button button8;
+        private Button button4;
+        private Button button1;
+        private Button button2;
+        private Button button7;
+        private Button button3;
+        private Button button6;
+        private Button button9;
+        private Button button0;
+        private GroupBox groupBox1;
+        private Button buttonC;
+        private Button buttondivisão;
+        private Button buttonvezes;
+        private Button buttonmenos;
+        private Button buttonsoma;
+        private Label Resultado_Text;
+        private GroupBox groupBox2;
+        private Button buttonenter;
+    }
+}
---
+++ TrabalhoAyres/TrabalhoAyres/FrmCalculadora.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoAyres
+{
+    public partial class FrmCalculadora : Form
+    {
+        double conta = 0, tipo = 0;
+        public FrmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double botao = 1;
+            Resultado_Text.Text += botao.ToString();
+        }
+
+        private void buttonsoma_Click(object sender, EventArgs e)
+        {
+            buttonsoma.Enabled = false;
+            buttonmenos.Enabled = false;
+            buttonvezes.Enabled = false;
+            buttondivisão.Enabled = false;
+            conta = Convert.ToDouble(Resultado_Text.Text);
+            Resultado_Text.Text = "";
+            tipo = 1;
+        }
+
+        private void buttonenter_Click(object sender, EventArgs e)
+        {
+            buttonvezes.Enabled = true;
+            buttonmenos.Enabled = true;
+            buttonsoma.Enabled = true;
+            buttondivisão.Enabled = true;
+            double resultado = 0;
+            if(Resultado_Text.Text == "")
+            {
+                MessageBox.Show("digite o segundo número");
+                buttonsoma.Enabled = false;
+                buttonmenos.Enabled = false;
+                buttonvezes.Enabled = false;
+                buttondivisão.Enabled = false;
+                return;
+
+            }
+
+            if (tipo == 1)
+            {
+                resultado = conta + Convert.ToDouble(Resultado_Text.Text);
+                Resultado_Text.Text = resultado.ToString();
+            }
+            if (tipo == 2)
+            {
+                resultado = conta - Convert.ToDouble(Resultado_Text.Text);
+                Resultado_Text.Text = resultado.ToString();
+            }
+            if (tipo == 3)
+            {
+                resultado = conta * Convert.ToDouble(Resultado_Text.Text);
+                Resultado_Text.Text = resultado.ToString();
+            }
+            if (tipo == 4)
+            {
+                resultado = conta / Convert.ToDouble(Resultado_Text.Text);
+                Resultado_Text.Text = resultado.ToString();
+            }
+            conta = Convert.ToDouble(Resultado_Text.Text);
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            Resultado_Text.Text = "";
+            conta = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double botao = 2;
+            Resultado_Text.Text += botao.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double botao = 3;
+            Resultado_Text.Text += botao.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double botao = 4;
+            Resultado_Text.Text += botao.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double botao = 5;
+            Resultado_Text.Text += botao.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double botao = 6;
+            Resultado_Text.Text += botao.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            double botao = 7;
+            Resultado_Text.Text += botao.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            double botao = 8;
+            Resultado_Text.Text += botao.ToString();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            double botao = 9;
+            Resultado_Text.Text += botao.ToString();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            double botao = 0;
+            Resultado_Text.Text += botao.ToString();
+        }
+
+        private void buttonmenos_Click(object sender, EventArgs e)
+        {
+            buttonmenos.Enabled = false;
+            buttonsoma.Enabled = false;
+            buttonvezes.Enabled = false;
+            buttondivisão.Enabled = false;
+            conta = Convert.ToDouble(Resultado_Text.Text);
+            Resultado_Text.Text = "";
+            tipo = 2;
+        }
+
+        private void buttonvezes_Click(object sender, EventArgs e)
+        {
+            buttonvezes.Enabled = false;
+            buttonmenos.Enabled = false;
+            buttonsoma.Enabled = false;
+            buttondivisão.Enabled = false;
+            conta = Convert.ToDouble(Resultado_Text.Text);
+            Resultado_Text.Text = "";
+            tipo = 3;
+        }
+
+        private void buttondivisão_Click(object sender, EventArgs e)
+        {
+            buttondivisão.Enabled = false;
+            buttonmenos.Enabled = false;
+            buttonvezes.Enabled = false;
+            buttonsoma.Enabled = false;
+            conta = Convert.ToDouble(Resultado_Text.Text);
+            Resultado_Text.Text = "";
+            tipo = 4;
+        }
+    }
+}
